3cfa4a9b6100751f4be81177fb0f3c279c7c3bb1
kimsunho2000 <sunhokim28@gmail.com>
2024-11-13 21:34:46 +0900

    이미지파일명,버튼 텍스트명 수정,가로모드 xml 레이아웃 완성 ,xml 인스턴스 코드에서 생성

--- app/src/main/java/com/example/assignment1/MainActivity.java
+++ app/src/main/java/com/example/assignment1/MainActivity.java
@@ -1,18 +1,37 @@
 package com.example.assignment1;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView1;
+    ImageView imageView2;
+    ImageView imageView3;
+    EditText editText;
+    Button button1;
+    Button button2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView1 = findViewById(R.id.imageView1);
+        imageView2 = findViewById(R.id.imageView2);
+        imageView3 = findViewById(R.id.imageView3);
+        editText = findViewById(R.id.editText);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        //default settings
+        editText.setText("1");
+        imageView1.setImageResource(R.drawable.image1);
+
+
     }
 }
